fdc7b0677bdd7526142c5f426b5bf43fb3a7e566
Artem Luksha <kiritorang11999@mail.ru>
2023-04-13 13:25:48 +0300

    Init v2.0

---
+++ src/Task1/src/main/java/FirstTask/App.java
@@ -0,0 +1,13 @@
+package FirstTask;
+
+/**
+ * Hello Артём!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Artem!" );
+    }
+}
